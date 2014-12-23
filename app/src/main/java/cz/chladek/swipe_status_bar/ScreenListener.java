0074f9cb7cce9bdfd785581bf23bccbea4aee92d
gerhardol <gerhard.nospam@gmail.com>
2014-12-24 00:14:50 +0100

    Setting GPLv2 on the project specific files

--- app/src/main/java/cz/chladek/swipe_status_bar/ScreenListener.java
+++ app/src/main/java/cz/chladek/swipe_status_bar/ScreenListener.java
@@ -1,3 +1,18 @@
+/* SwipeStatusBar  - Access the Status Bar Anywhere, Anytime
+        Copyright (C) 2013 Thomas.
+
+        This file is part of SwipeStatusBar.
+        SwipeStatusBar is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 2 of the License, or
+        (at your option) any later version.
+        SwipeStatusBar is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+        GNU General Public License for more details.
+        You should have received a copy of the GNU General Public License
+        along with SwipeStatusBar. If not, see <http://www.gnu.org/licenses/>.
+*/
 package cz.chladek.swipe_status_bar;
 
 import android.content.BroadcastReceiver;
